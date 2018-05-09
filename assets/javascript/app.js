0cd688c08b0bb95dadaf6f429b897594be426ed0
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-05-08 22:00:43 -0700

    changing the format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,104 @@
 $(document).ready(function () {
-    console.log("Let's get started!");
+
+    console.log("Lettt's get started!");
 
     //inside the var topics put your selected theme
     var topics = [
-        "Streching",
         "Swimming",
+        "Kickboxing",
         "Snow-boarding",
-        "Cartoon-Workout",
+        "Funny-Workout",
         "Dancing"
     ];
     // this is the limitation of how many pic you wanna show on the screen with each click/per topic
     // var picsPerTopic = 10;
     // var topicButtons = [];
-    var picButtons = [];
+    // var picButtons = [];
 
+    //this is the main function starting the page
 
-    function giftastic() {
+    $(document).on("click", ".gif", function () {
 
-        setupPage();
-    }
+        var state = $(this).attr("data-state");
+        console.log(state);
 
+        //Check if the variable state is equal to 'still',
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate")
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    })
+
+    // //this is the setup function:
     function setupPage() {
 
         generateTopicSection();
-        //addNewTopic();
+        // addNewTopic();
     }
-    //generate topic section with all the buttons
+
+
+    // //generate topic section with all the buttons
     function generateTopicSection() {
         // Deletes the buttons prior to adding new buttons
         // (this is necessary otherwise you will have repeat buttons)
+
         $("#topic-section").empty();
         //looping through the array of topics
 
-        for (i = 0; i < topics.length; i++) {
+        for (var i = 0; i < topics.length; i++) {
+
             //creating new button for each item in topics array
             var newButton = $("<button>")
+
             //Add a class to our button
             newButton.addClass("topic-btn")
+            newButton.attr("id", "topic-btn" + i)
             // Add a data-attribute
             newButton.attr("data-name", topics[i])
             // Provid the initial button text
             newButton.text(topics[i]);
             // Add the button to the topic-section div
+            // console.log("adding onclick for button i:", i, " attribute:", newButton.attr("data-name"))
+
+            // console.log("test", newButton.attr("id"))
+            // console.log("test", $(newButton.attr("id")).attr("data-name"))
+            //now add this new button to the list of topicButtons:
+            // topicButtons.push(newButton);
             $("#topic-section").append(newButton);
         }
-
     }
+
+    $(document).on("click", ".topic-btn", function (event) {
+        event.preventDefault();
+        var topics = $(this).attr("data-name");
+        // console.log(topic)
+        // This line of code will grab the input from the textbox
+        getDispalyPics(topics)
+    });
+
     //add the text box/new topic to the screen
     $("#add-topic").on("click", function (event) {
         event.preventDefault();
+
         // This line of code will grab the input from the textbox
-        var topicButtons = $("#validationCustom01").val().trim();
-
+        var newTopicText = $("#validationCustom01").val().trim();
+        // console.log(newTopicText );
         // The new topic from the textbox is then added to our array
-        topics.push(topicButtons);
+        topics.push(newTopicText);
 
-        // Calling renderButtons which handles the processing of our movie array
         generateTopicSection();
     });
 
 
-    function getDispalyPics() {
 
-        var queryURL = "https://api.giphy.com/v1/gifs/trending?q=" + topics + "&api_key=cDzVAOKFADbFNgyeKftxDbwnHSSovoPU&limit=10&rating=G";
+
+    function getDispalyPics(topics) {
+        // console.log("getDisplayPics running with topic:", topic)
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cDzVAOKFADbFNgyeKftxDbwnHSSovoPU&q=" + topics + "&limit=10"
 
         $.ajax({
             url: queryURL,
@@ -70,18 +106,50 @@ $(document).ready(function () {
         }).then(function (response) {
 
             console.log(response);
-            console.log(queryURL);
-
+            // console.log(queryURL);
+            var results = response.data;
+            for (i = 0; i < results.length; i++) {
+                var stillGif = results[i].images.original_still.url
+                // console.log("the still gif: ", stillGif);
+                var animated = results[i].images.original.url
+                // console.log("test the animated:", results[i].images.original.url)
+                var rating = results[i].rating
+                // console.log("test the rating:", results[i].rating)
+                // Make a div with jQuery and store it in a variable named picSection.
+                var picSectionDiv = $("<div>");
+                // Make a paragraph tag with jQuery and store it in a variable named p.
+                var p = $("<p>");
+                // set the inner text of the paragraph to the rating of the image
+                p.text("Rating: " + rating);
+                //make an image tag and store it in a variable call searchImage.
+                var searchImage = $("<img>");
+                //set the image's src to result[i]'s stillGif
+                searchImage.attr("src", stillGif)
+                searchImage.attr("data-animate", animated)
+                searchImage.attr("data-still", stillGif)
+                searchImage.attr("data-state", "still")
+                searchImage.attr("class", "gif")
+                //append the p variable to the picSection div
+                picSectionDiv.append(p);
+                //append the searchImage variable to the picSection div
+                picSectionDiv.append(searchImage);
+                // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
+                $("#gifs-appear-here").prepend(picSectionDiv);
+            }
 
 
         });
 
     };
-    // Adding click event listeners to all elements with a class of "topic-btn"
-    $(document).on("click", ".topic-btn", getDispalyPics);
 
-    // Calling the generateTopicSecton function to display the intial buttons
-    generateTopicSection();
-
-    getDispalyPics();
+    //let's call the main function to start the page:
+    setupPage();
 });
+
+
+// newButton.on("click", function (event) {
+//     event.preventDefault();
+//     console.log(" onclick data-name:", newButton.attr("data-name"))
+//     console.log(" onclick text:", newButton.text())
+//     getDispalyPics(newButton.attr("data-name"));
+// });
